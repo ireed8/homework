c00cda1aa204be5109773e0dbadee803f257a373
Ivan Reed <ivanp.reed@gmail.com>
2021-07-11 13:50:29 -0500

    homework harder

--- classActivity/2.3.8-ForLoops.js
+++ classActivity/2.3.8-ForLoops.js
@@ -1,6 +1,6 @@
 let total = 0;
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 10; i++) {
     total++;
     console.log(total);
 };
--- classActivity/3.1.1-FunctionDeclaration.js
+++ classActivity/3.1.1-FunctionDeclaration.js
@@ -1,5 +1,12 @@
-let greet = function() {
-    console.log("Hello");
-};
-
-console.log(greet());
+// a function with 4 parameters, param4 has a default value
+function myFunction(param1 = "large", param2 = "thin", param3 = "toppings", param4 = "One large thick crust pizza with x, y, z, ... coming up!") {
+    // logging each param
+    console.log(param1, param2, param3);
+    console.log(param4);
+  }
+  
+  // passing in only 1 argument to our function
+  myFunction();
+  // prints:
+  // 2 undefined
+  // undefined a
