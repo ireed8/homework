e8d93d62fd34144c9822081977a992f6bd3d79f5
Ivan Reed <ivanp.reed@gmail.com>
2021-07-11 18:05:54 -0500

    chapter class activies

---
+++ WeekThreeHomeWork.js
@@ -0,0 +1,11 @@
+// param1 & param2 are parameters
+function myFunction(ham, cheese) {
+    // printing each parameter
+    console.log(ham);
+    console.log(cheese);
+  }
+  // 1 & 2 are arguments passed into the function
+  myFunction(1, 2);
+  // prints:
+  // 1
+  // 2
